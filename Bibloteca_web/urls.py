9ec277837ac5d322d56b5da5164bfcb0f8883ee4
CrisCrio <jaramartinezcristianesteban@gmail.com>
2026-02-22 14:58:00 -0500

    actualisacion de las urls

--- Bibloteca_web/urls.py
+++ Bibloteca_web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+#para poder importar todas las funciones de views
 from . import views
 
-urlpatterns =[
-    #Asociar la función a la vista con url /registro/
+urlpatterns = [
+    # Asociar la funcion a la vista con url /registro/
     path('registro/',views.registro_usuario, name='registro'),
     path('login/', views.iniciar_sesion, name='login'),
     path('cerrar_sesion/', views.cerrar_sesion, name='cerrar_sesion'),
@@ -10,6 +11,6 @@ urlpatterns =[
     
     path('libros/', views.listar_recerva, name='listar_recerva'),
     path('libros/reserva/', views.crear_reserva, name='crear_recerva'),
-    path('libros/eliminar/<str:recerba_id>/', views.eliminar_reserva, name='eliminar_recerva'),
-    path('libros/editar/<str:recerba_id>/', views.editar_recerva, name='editar_recerva')
+    path('libros/eliminar/<str:reserva_id>/', views.eliminar_reserva, name='eliminar_reserva'),
+    path('libros/editar/<str:reserva_id>/', views.editar_recerva, name='editar_reserva')
 ]
